11089ed17cf9940dde3768424ceb95145cb57c5d
Brian Kleinberg <briankleinberg@MacBook-Pro.local>
2022-07-26 09:39:22 -0500

    added migrations folder + made class for models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+  title = models.CharField(max_length=255)
+  description = models.CharField(max_length=1000)
+  price = models.DecimalField(max_digits=8, decimal_places=2)
+  inventory_quantity = models.IntegerField()
+  url = models.CharField(max_length=255)
+
+class Review(models.Model):
+    review = models.TextField()
+    date_added = models.DateField(("Date"), auto_now_add = True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null=True)  
